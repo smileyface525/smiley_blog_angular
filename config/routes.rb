570868ffe157f78f04a9ed82188737592aa22532
Eiko Seino <eikoseino@hotmail.com>
2014-10-23 20:03:46 -0700

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :blogs, except: [:show, :new]
+  resources :tags, only: [:index]
+  resources :sessions, except: [:index, :create, :destroy]
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
