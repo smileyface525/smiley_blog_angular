4dc2e0e90d7854a3f0408c41801a78f3f54ade78
Eiko Seino <eikoseino@hotmail.com>
2014-10-23 20:03:31 -0700

    Adds before_save callbacks: titleize_name and titleize_title

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,10 @@ class Blog < ActiveRecord::Base
   validates :title, :content, :tags, presence: true
   validates :title, length: { maximum: 30,
       too_long: "%{count} characters is the maximum allowed" }
+  before_save :titleize_title!
+
+  def titleize_title!
+    self.title = title.titleize
+  end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,13 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :blogs
-  validates :name, :blogs, presence: true
+  validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { maximum: 10,
       too_long: "%{count} characters is the maximum allowed" }
+  before_save :titleize_name!
+
+  def titleize_name!
+    self.name = name.titleize
+  end
+
 end
