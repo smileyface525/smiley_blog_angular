b7e310a2d46724546ad6736a6479a714ff3ae124
Eiko Seino <eikoseino@hotmail.com>
2014-11-02 18:32:19 -0800

    adds extra protection from CSRF.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :reset_session
+  protect_from_forgery
   skip_before_action :verify_authenticity_token, if: :json_request?
+  after_filter :set_csrf_cookie_for_ng
 
 
   helper_method :current_user, :logged_in?
@@ -15,10 +16,18 @@ class ApplicationController < ActionController::Base
     current_user != nil
   end
 
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
   protected
 
   def json_request?
     request.format.json?
   end
+
+  def verified_request?
+      super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+    end
 
 end
