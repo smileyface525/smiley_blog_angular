5e7d5a0a32974a92599ac3edf93a7b344a35ce2c
Eiko Seino <eikoseino@hotmail.com>
2014-11-08 13:02:06 -0800

    Adds blog.tags extension to update tags Adds callback method that gets called right before Blog object gets deleted to make sure that tags that will be empty once the object gets deleted will be deleted. Sets default values to the order of blogs and comments

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,31 +1,35 @@
 class Blog < ActiveRecord::Base
-  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :tags,
+    -> { extending TagsExtension },
+    after_remove: :destroy_empty_tag
+
   has_many :comments
   validates :title, :content, :tags, presence: true
   validates :title, length: { maximum: 30,
       too_long: "%{count} characters is the maximum allowed" }
   before_save :titleize_title!
-  before_destroy :delete_empty_tags
-  DEFAULT_ORDER = {created_at: :desc}
+  before_destroy :destroy_empty_tags
+  DEFAULT_BLOGS_ORDER = {created_at: :desc}
+  DEFAULT_COMMNETS_ORDER = {created_at: :desc}
 
   def self.all_with_tags_and_comments
-    all.order(DEFAULT_ORDER).map(&:with_tags_and_comments)
+    all.order(DEFAULT_BLOGS_ORDER).map(&:with_tags_and_comments)
   end
 
   def self.recent_with_tags_and_comments(limit)
-    blogs = self.all.order(DEFAULT_ORDER).limit(limit)
+    blogs = self.all.order(DEFAULT_BLOGS_ORDER).limit(limit)
     blogs.map(&:with_tags_and_comments)
   end
 
   def self.with_tags_and_comments_for(tag)
     blogs = Tag.find_by_name(tag).try(:blogs) || self.all
-    blogs.order(DEFAULT_ORDER).map(&:with_tags_and_comments)
+    blogs.order(DEFAULT_BLOGS_ORDER).map(&:with_tags_and_comments)
   end
 
   def with_tags_and_comments
       {blog: self,
        tags: self.tags.map(&:name),
-       comments: self.comments.order(created_at: :desc).map{ |c| {
+       comments: self.comments.order(DEFAULT_COMMNETS_ORDER).map{ |c| {
           id: c.id,
           content: c.content,
           reply: c.reply,
@@ -38,9 +42,26 @@ class Blog < ActiveRecord::Base
     self.title = title.titleize
   end
 
-  def delete_empty_tags
+  def destroy_empty_tag(tag)
+    tag.destroy if tag.blogs.count == 0
+  end
+
+  def destroy_empty_tags
     tags.each do |tag|
       tag.destroy if tag.blogs.count == 1
+    end
+  end
+
+end
+
+module TagsExtension
+
+  def update_tags_with(new_tags)
+    new_tags.each do |tag|
+      self << tag if !self.include?(tag)
+    end
+    self.each do |tag|
+      self.destroy(tag) if !new_tags.include?(tag)
     end
   end
 
